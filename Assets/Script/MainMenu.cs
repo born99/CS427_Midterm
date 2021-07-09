14b548c87b5a84fa38d2876e16b0bd31dad881e8
Đỗ Vương Quốc Thịnh <37610310+dvqthinh25111999@users.noreply.github.com>
2021-07-09 16:59:03 +0700

    Update Main Menu

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class MainMenu : MonoBehaviour
 {
+    public Slider[] volumeSliders;
+    public Toggle[] resolutionToggles;
+    public int[] screenWidths;
+    int activeScreenResIndex;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -14,5 +20,48 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("QUIT!");
         Application.Quit();
+    }
+
+    public void SetScreenResolution(int i)
+    {
+        if (resolutionToggles[i].isOn)
+        {
+            activeScreenResIndex = i;
+            float aspectRatio = 16 / 9f;
+            Screen.SetResolution(screenWidths[i], (int)(screenWidths[i] / aspectRatio), false);
+        }
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        for(int i = 0; i < resolutionToggles.Length; i++)
+        {
+            resolutionToggles[i].interactable = !isFullScreen;
+            if (isFullScreen)
+            {
+                Resolution[] allResolutions = Screen.resolutions;
+                Resolution maxResolution = allResolutions[allResolutions.Length - 1];
+                Screen.SetResolution(maxResolution.width, maxResolution.height, true);
+            }
+            else
+            {
+                SetScreenResolution(activeScreenResIndex);
+            }
+        }
+    }
+
+    public void SetMasterVolume(float value)
+    {
+
+    }
+
+    public void SetMusicVolume(float value)
+    {
+
+    }
+
+    public void SetSfxVolume(float value)
+    {
+
     }
 }
