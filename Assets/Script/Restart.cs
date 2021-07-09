0544384e2c0a713ef9f197c6703be3cbad2723c0
Nguyễn Minh Quân <56881880+zxquan123@users.noreply.github.com>
2021-07-09 08:31:40 +0700

    ..

--- Assets/Script/Restart.cs
+++ Assets/Script/Restart.cs
@@ -5,15 +5,17 @@ using UnityEngine.SceneManagement;
 public class Restart : MonoBehaviour
 {
     // Start is called before the first frame update
-   
 
+    public GameObject clicksound;
     // Update is called once per frame
     public void PlayGame()
     {
+       
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     public void QuitGame()
     {
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1);
     }
 }
